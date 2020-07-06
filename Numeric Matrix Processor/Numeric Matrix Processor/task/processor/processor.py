88245a3bbca0ab9f6c204f6d4f5dcfd748a4b8ef
dmiwer <dmiwer@gmail.com>
2020-07-06 05:39:27 +0300

    Numeric Matrix Processor 2/6

--- Numeric Matrix Processor/Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/Numeric Matrix Processor/task/processor/processor.py
@@ -1,11 +1,16 @@
 ax, ay = [int(x) for x in input().split()]
 m1 = [[int(y) for y in input().split()] for _ in range(ax)]
 
-bx, by = [int(x) for x in input().split()]
-m2 = [[int(y) for y in input().split()] for _ in range(bx)]
+# bx, by = [int(x) for x in input().split()]
+# m2 = [[int(y) for y in input().split()] for _ in range(bx)]
+#
+# if ax == bx and ay == by:
+#     for x in range(ax):
+#         print(" ".join(str(m1[x][y] + m2[x][y]) for y in range(ay)))
+# else:
+#     print("ERROR")
 
-if ax == bx and ay == by:
-    for x in range(ax):
-        print(" ".join(str(m1[x][y] + m2[x][y]) for y in range(ay)))
-else:
-    print("ERROR")
+c = int(input())
+
+for x in range(ax):
+    print(*[m1[x][y] * c for y in range(ay)])
