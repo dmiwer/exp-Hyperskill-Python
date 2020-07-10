883a70839faad5d9a491f9510868bd16b8d6de47
dmiwer <dmiwer@gmail.com>
2020-07-10 08:45:04 +0300

    Numeric Matrix Processor 5/6

--- Numeric Matrix Processor/Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/Numeric Matrix Processor/task/processor/processor.py
@@ -10,9 +10,28 @@ class Matrix:
         ans.arr = arr
         return ans
 
+    @staticmethod
+    def cof(row, col, arr):
+        m = []
+        for j in range(len(arr)):
+            if j == row: continue
+            t = []
+            for i in range(len(arr[0])):
+                if i == col: continue
+                t.append(arr[j][i])
+            m.append(t)
+        ans = Matrix.det(m)
+        return ans if (row + col) % 2 == 0 else -ans
+
+    @staticmethod
+    def det(arr):
+        if len(arr) == 2:
+            return arr[0][0] * arr[1][1] - arr[0][1] * arr[1][0]
+        return sum([arr[0][i] * Matrix.cof(0, i, arr) for i in range(len(arr[0]))])
+
     def show(self):
-        for h in range(self.height):
-            print(*[self.arr[h][l] for l in range(self.length)])
+        for i in range(self.height):
+            print(*[self.arr[i][j] for j in range(self.length)])
 
     def fill(self):
         temp = []
@@ -24,13 +43,16 @@ class Matrix:
         self.arr = temp
 
     def sum(self, other):
-        return Matrix.get([[(self.arr[i][j] + other.arr[i][j]) for j in range(self.length)] for i in range(self.height)])
+        return Matrix.get(
+            [[(self.arr[i][j] + other.arr[i][j]) for j in range(self.length)] for i in range(self.height)])
 
     def mul(self, other):
         if type(other) == type(2.3):
             return Matrix.get([[(self.arr[i][j] * other) for j in range(self.length)] for i in range(self.height)])
         elif type(other) == type(self):
-            return Matrix.get([[sum(self.arr[i][x] * other.arr[x][j] for x in range(self.length)) for j in range(other.length)] for i in range(self.height)])
+            return Matrix.get(
+                [[sum(self.arr[i][x] * other.arr[x][j] for x in range(self.length)) for j in range(other.length)] for i
+                 in range(self.height)])
         else:
             return -1
 
@@ -38,13 +60,19 @@ class Matrix:
         if n == 1:
             return Matrix.get([[self.arr[j][i] for j in range(self.length)] for i in range(self.height)])
         elif n == 2:
-            return Matrix.get([[self.arr[j][i] for j in reversed(range(self.length))] for i in reversed(range(self.height))])
+            return Matrix.get(
+                [[self.arr[j][i] for j in reversed(range(self.length))] for i in reversed(range(self.height))])
         elif n == 3:
             return Matrix.get([[self.arr[i][j] for j in reversed(range(self.length))] for i in range(self.height)])
         elif n == 4:
             return Matrix.get([[self.arr[i][j] for j in range(self.length)] for i in reversed(range(self.height))])
         else:
             return -1
+
+    def determinant(self):
+        if self.height == 0: return 0
+        if self.height == 1: return self.arr[0][0]
+        return Matrix.det(self.arr)
 
 
 def add_matrices():
@@ -53,14 +81,14 @@ def add_matrices():
     m1 = Matrix(m1_height, m1_length)
     m1.fill()
     m2_height, m2_length = map(int, input("Enter size of second matrix: ").split(" "))
-    print("Enter second matrix: ")
     if m1_height != m2_height or m1_length != m2_length:
         show_err()
         return
+    print("Enter second matrix: ")
     m2 = Matrix(m2_height, m2_length)
     m2.fill()
-    res_matrix = m1.sum(m2)
     print("The result is:")
+    res_matrix = m1.sum(m2)
     res_matrix.show()
 
 
@@ -70,14 +98,14 @@ def mul_by_matrix():
     m1 = Matrix(m1_height, m1_length)
     m1.fill()
     m2_height, m2_length = map(int, input("Enter size of second matrix: ").split(" "))
-    print("Enter second matrix: ")
     if m1_length != m2_height:
         show_err()
         return
+    print("Enter second matrix: ")
     m2 = Matrix(m2_height, m2_length)
     m2.fill()
-    res_matrix = m1.mul(m2)
     print("The result is:")
+    res_matrix = m1.mul(m2)
     res_matrix.show()
 
 
@@ -87,9 +115,26 @@ def mul_by_const():
     m1 = Matrix(m1_height, m1_length)
     m1.fill()
     c = float(input("Enter constant: "))
-    res_matrix = m1.mul(c)
     print("The result is:")
+    res_matrix = m1.mul(c)
     res_matrix.show()
+
+
+def calc_det():
+    m1_height, m1_length = map(int, input("Enter matrix size: ").split(" "))
+    m1 = Matrix(m1_height, m1_length)
+    if m1_height != m1_length:
+        show_err()
+        return
+    print("Enter matrix: ")
+    m1.fill()
+    print("The result is: ")
+    result = m1.determinant()
+    print(result)
+
+
+def show_err():
+    print("The operation cannot be performed.")
 
 
 def transpose_matrix():
@@ -103,40 +148,24 @@ Your choice: """))
     print("Enter matrix: ")
     m1 = Matrix(m1_height, m1_length)
     m1.fill()
-    res_matrix = m1.transpose(n)
     print("The result is:")
+    res_matrix = m1.transpose(n)
     res_matrix.show()
 
 
-def get_choice():
-    return int(input("""
+while True:
+    choice = int(input("""
 1. Add matrices
 2. Multiply matrix by a constant
 3. Multiply matrices
 4. Transpose matrix
+5. Calculate a determinant
 0. Exit
 Your choice: """.lstrip()))
-
-
-def show_err():
-    print("The operation cannot be performed.")
-
-
-def execute(choice):
-    if choice == 1:
-        add_matrices()
-    elif choice == 2:
-        mul_by_const()
-    elif choice == 3:
-        mul_by_matrix()
-    elif choice == 4:
-        transpose_matrix()
-    return
-
-
-while True:
-    choice = get_choice()
-    if choice == 0:
-        break
-    execute(choice)
+    if choice == 0:     break
+    elif choice == 1:   add_matrices()
+    elif choice == 2:   mul_by_const()
+    elif choice == 3:   mul_by_matrix()
+    elif choice == 4:   transpose_matrix()
+    elif choice == 5:   calc_det()
     print()
